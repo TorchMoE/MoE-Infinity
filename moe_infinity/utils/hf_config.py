5e096b402303c57fab5b9122252376869a81e9dd
Leyang Xue <s2062808@ed.ac.uk>
2024-06-02 16:46:23 +0100

    Arctic Support (#18)
    * init
    
    * fix:add tokenizer for arctic in inference_example
    
    * fix import dir
    
    * fix mixtral block
    
    * feat:update arctic model version
    
    ---------
    
    Co-authored-by: xly <leyang.xue@ed.ac.uk>
    Co-authored-by: lausannel <513964121@qq.com>

--- moe_infinity/utils/hf_config.py
+++ moe_infinity/utils/hf_config.py
@@ -1,5 +1,4 @@
-from moe_infinity.utils.constants import MODEL_MAPPING_TYPES, MODEL_MAPPING_NAMES
-from transformers import PretrainedConfig, MixtralForCausalLM
+from transformers import PretrainedConfig
 from typing import Tuple
 import re
 import torch
@@ -18,24 +17,6 @@ def parse_expert_dtype(config: PretrainedConfig) -> int:
 
     return dtype
 
-def parse_expert_type(config: PretrainedConfig) -> int:
-    arch = config.architectures[0].lower()
-    if "switch" in arch:
-        return MODEL_MAPPING_TYPES["switch"]
-    elif "nllb" in arch:
-        return MODEL_MAPPING_TYPES["nllb"]
-    elif "mixtral" in arch:
-        return MODEL_MAPPING_TYPES["mixtral"]
-    elif "grok" in arch:
-        return MODEL_MAPPING_TYPES["grok"]
-    # elif "opt" in arch:
-    #     return 0
-    else:
-        raise RuntimeError(f"Unsupported architecture {arch}")
-
-    return MODEL_MAPPING_TYPES[arch]
-
-
 def parse_moe_param(config: PretrainedConfig) -> Tuple[int, int, int]:
     arch = config.architectures[0].lower()
 
@@ -49,16 +30,11 @@ def parse_moe_param(config: PretrainedConfig) -> Tuple[int, int, int]:
         num_decoder_layers = config.decoder_layers // config.decoder_sparse_step
         num_layers = num_encoder_layers + num_decoder_layers
         num_experts = config.num_experts
-    elif "mixtral" in arch:
+    elif "mixtral" in arch or "arctic" in arch:
         num_encoder_layers = 0
         num_decoder_layers = config.num_hidden_layers
         num_layers = config.num_hidden_layers
         num_experts = config.num_local_experts
-    # elif "opt" in arch:
-    #     num_encoder_layers = 0
-    #     num_decoder_layers = config.num_hidden_layers
-    #     num_layers = num_encoder_layers + num_decoder_layers
-    #     num_experts = 0
     elif "grok" in arch:
         num_encoder_layers = 0
         num_decoder_layers = config.num_hidden_layers
@@ -88,7 +64,7 @@ def parse_expert_id(param_name: str, config: PretrainedConfig) -> Tuple[int, int
             layer_id = int(layer_id)
             expert_id = int(expert_id)
 
-    elif "mixtral" in arch:
+    elif "mixtral" in arch or "arctic" in arch:
         encoder_sparse_step = None
         decoder_sparse_step = 1
         layer_type = "decoder"
